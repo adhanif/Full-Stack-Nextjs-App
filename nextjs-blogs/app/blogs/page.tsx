8856273a3a1b9a084637168d5235d11355d69b40
adhanif <ad.hanif@outlook.com>
2026-09-06 21:28:15 +0300

    Exercise: 6. Search completed

--- nextjs-blogs/app/blogs/page.tsx
+++ nextjs-blogs/app/blogs/page.tsx
@@ -1,14 +1,35 @@
-import Link from 'next/dist/client/link';
+import Link from 'next/link';
 import { getBlogs } from '../services/blogs';
 
-const Blogs = () => {
-  const blogs = getBlogs();
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) => {
+  const { filter } = await searchParams;
 
-  const descendingOrder = blogs.sort((a, b) => b.likes - a.likes);
- 
+  const blogs = await getBlogs();
+
+  const filteredBlogs = filter
+    ? blogs.filter((blog) =>
+        blog.title.toLowerCase().includes(filter.toLowerCase()),
+      )
+    : blogs;
+  const descendingOrder = [...filteredBlogs].sort((a, b) => b.likes - a.likes);
+
   return (
     <div>
       <h1>Blogs</h1>
+      <form method="GET">
+        <input
+          type="text"
+          name="filter"
+          placeholder="Search blogs by title"
+          defaultValue={filter || ''}
+        />
+
+        <button type="submit">Search</button>
+      </form>
       <ul>
         {descendingOrder.map((blog) => (
           <li key={blog.id}>
