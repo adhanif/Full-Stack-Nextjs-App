6ce9ec348f5194f0884fc73decea5a1e518bbe58
adhanif <ad.hanif@outlook.com>
2026-09-12 17:32:19 +0300

    Exercise:9. Users + Exercise: 10. User page completed

--- nextjs-blogs/app/services/blogs.ts
+++ nextjs-blogs/app/services/blogs.ts
@@ -2,13 +2,25 @@ import { desc, eq, sql } from 'drizzle-orm';
 import { db } from '../../db';
 import { blogs } from '../../db/schema';
 
-export const addBlog = (title: string, author: string, url: string) => {
-  return db.insert(blogs).values({ title, author, url, likes: 0 });
+export const addBlog = async (title: string, author: string, url: string) => {
+  const user = await db.query.users.findFirst({
+    orderBy: sql`RANDOM()`,
+  });
+
+  if (!user) {
+    throw new Error('No users found to attach the new blog to.');
+  }
+
+  return db
+    .insert(blogs)
+    .values({ title, author, url, likes: 0, userId: user.id });
 };
-export const getBlogById = (id: number) => {
+
+export const getBlogById = async (id: number) => {
   return db.query.blogs.findFirst({ where: eq(blogs.id, id) });
 };
-export const increaseLikes = (id: number) => {
+
+export const increaseLikes = async (id: number) => {
   return db
     .update(blogs)
     .set({ likes: sql`${blogs.likes} + 1` })
